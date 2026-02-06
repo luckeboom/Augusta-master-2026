09f18d65b42599b624c1690db1eef027518cca1c
luckeboom <lucketell2003@gmail.com>
2026-02-06 17:51:16 +0100

    Create navigation.js

---
+++ navigation.js
@@ -0,0 +1,9 @@
+function showGame() {
+  document.getElementById("stdout").style.display = "block";
+  document.getElementById("leaderboard").style.display = "none";
+}
+
+function showLeaderboard() {
+  document.getElementById("stdout").style.display = "none";
+  document.getElementById("leaderboard").style.display = "block";
+}
