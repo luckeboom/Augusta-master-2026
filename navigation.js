02c39880f552ed37bd046233ca8572cdc8750710
luckeboom <lucketell2003@gmail.com>
2026-03-22 14:41:10 +0100

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,10 +1,1 @@
-function showGame() {
-  document.getElementById("stdout").style.display = "block";
-  document.getElementById("leaderboard").style.display = "none";
-}
 
-function showLeaderboard() {
-  document.getElementById("stdout").style.display = "none";
-  document.getElementById("leaderboard").style.display = "block";
-  loadLeaderboard();
-}
